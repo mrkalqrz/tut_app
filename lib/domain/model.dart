393d619f0caaa8b69af07ce1c42f43ba04ce40c6
mrkalqrz <maki.alquiroz@gmail.com>
2022-12-19 17:51:58 +0800

    finished lesson 48

--- lib/domain/model.dart
+++ lib/domain/model.dart
@@ -5,3 +5,26 @@ class SliderObject {
 
   SliderObject(this.title, this.subtitle, this.image);
 }
+
+class Customer {
+  String id;
+  String name;
+  int numberOfNotifications;
+
+  Customer(this.id, this.name, this.numberOfNotifications);
+}
+
+class Contacts {
+  String phone;
+  String email;
+  String link;
+
+  Contacts(this.phone, this.email, this.link);
+}
+
+class Authentication {
+  Customer customer;
+  Contacts contacts;
+
+  Authentication(this.customer, this.contacts);
+}
