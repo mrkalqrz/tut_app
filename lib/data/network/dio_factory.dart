0790c2722650fa754b9013142a559446316db565
mrkalqrz <maki.alquiroz@gmail.com>
2022-12-28 09:34:58 +0800

    Lecture 66 - Update Dio Factory with App Preferences

--- lib/data/network/dio_factory.dart
+++ lib/data/network/dio_factory.dart
@@ -1,5 +1,6 @@
 import 'package:dio/dio.dart';
 import 'package:flutter/foundation.dart';
+import 'package:flutter_application_sample/app/app_prefs.dart';
 import 'package:flutter_application_sample/app/constant.dart';
 import 'package:pretty_dio_logger/pretty_dio_logger.dart';
 
@@ -10,15 +11,19 @@ const String AUTHORIZATION = "authorization";
 const String DEFAULT_LANGUAGE = "language";
 
 class DioFactory {
+  AppPreferences _appPreferences;
+  DioFactory(this._appPreferences);
+
   Future<Dio> getDio() async {
     Dio dio = Dio();
     int _timeout = 60 * 1000;
+    String language = await _appPreferences.getAppLanguage();
 
     Map<String, String> headers = {
       CONTENT_TYPE: APPLICATION_JSON,
       ACCEPT: APPLICATION_JSON,
       AUTHORIZATION: Constant.token,
-      DEFAULT_LANGUAGE: "en", // to do get language from apps pref
+      DEFAULT_LANGUAGE: language, // to do get language from apps pref
     };
 
     dio.options = BaseOptions(
