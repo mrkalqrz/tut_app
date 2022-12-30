5f4a79090b868d00efc90f520a865e9740bae49f
mrkalqrz <maki.alquiroz@gmail.com>
2022-12-30 11:59:16 +0800

    Lecture 103 - Handling showing many popup dialogs

--- lib/presentation/common/state_renderer/state_render_impl.dart
+++ lib/presentation/common/state_renderer/state_render_impl.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/material.dart';
 import 'package:flutter_application_sample/data/mapper/mapper.dart';
 import 'package:flutter_application_sample/presentation/common/state_renderer/state_renderer.dart';
 import 'package:flutter_application_sample/presentation/resources/strings_manager.dart';
@@ -64,4 +65,79 @@ class EmptyState extends FlowState {
   @override
   StateRendererType getStateRendererType() =>
       StateRendererType.EMPTY_SCREEN_STATE;
+}
+
+extension FlowStateExtension on FlowState {
+  Widget getScreenWidget(BuildContext context, Widget contentScreenWidget,
+      Function retryActionFunction) {
+    switch (this.runtimeType) {
+      case LoadingState:
+        {
+          if (getStateRendererType() == StateRendererType.POPUP_LOADING_STATE) {
+            //showing popup dialog
+            showPopup(context, getStateRendererType(), getMessage());
+
+            //return the content ui of the screen
+            return contentScreenWidget;
+          } else //StateRendererType.FULL_SCREEN_LOADING_STATE
+          {
+            return StateRender(
+                stateRendererType: getStateRendererType(),
+                retryActionFunction: retryActionFunction);
+          }
+        }
+      case ErrorState:
+        {
+          dismissDialog(context);
+          if (getStateRendererType() == StateRendererType.POPUP_ERROR_STATE) {
+            //showing popup dialog
+            showPopup(context, getStateRendererType(), getMessage());
+
+            //return the content ui of the screen
+            return contentScreenWidget;
+          } else //StateRendererType.FULL_SCREEN_ERROR_STATE
+          {
+            return StateRender(
+                stateRendererType: getStateRendererType(),
+                retryActionFunction: retryActionFunction);
+          }
+        }
+      case ContentState:
+        {
+          dismissDialog(context);
+          return contentScreenWidget;
+        }
+      case EmptyState:
+        {
+          return StateRender(
+              stateRendererType: getStateRendererType(),
+              message: getMessage(),
+              retryActionFunction: retryActionFunction);
+        }
+      default:
+        {
+          return contentScreenWidget;
+        }
+    }
+  }
+
+  dismissDialog(BuildContext context) {
+    if (_isThereCurrentDialogShowing(context)) {
+      Navigator.of(context, rootNavigator: true).pop(true);
+    }
+  }
+
+  _isThereCurrentDialogShowing(BuildContext context) =>
+      ModalRoute.of(context)?.isCurrent != true;
+
+  showPopup(BuildContext context, StateRendererType stateRendererType,
+      String message) {
+    WidgetsBinding.instance?.addPostFrameCallback((_) => showDialog(
+        context: context,
+        builder: (BuildContext context) => StateRender(
+              stateRendererType: stateRendererType,
+              message: message,
+              retryActionFunction: () {},
+            )));
+  }
 }
