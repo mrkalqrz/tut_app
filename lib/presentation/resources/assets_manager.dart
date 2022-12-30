08d0e20c20f2c8c7f071c5b2a5460a8a6fdf41a7
mrkalqrz <maki.alquiroz@gmail.com>
2022-12-30 10:52:45 +0800

    Lecture 98 - Applying State Renderer - Loading State

--- lib/presentation/resources/assets_manager.dart
+++ lib/presentation/resources/assets_manager.dart
@@ -1,4 +1,5 @@
 const String IMAGE_PATH = "assets/images";
+const String JSON_PATH = "assets/json";
 
 class ImageAssets {
   static const String splashLogo = "$IMAGE_PATH/splash_logo.png";
@@ -17,4 +18,11 @@ class ImageAssets {
   static const String settingsRightArrowIc =
       "$IMAGE_PATH/settings_right_arrow_ic.svg";
   static const String solidCircleIc = "$IMAGE_PATH/solid_circle_ic.svg";
+}
+
+class JsonAssets {
+  static const String loading = "$JSON_PATH/loading.json";
+  static const String empty = "$JSON_PATH/empty.json";
+  static const String error = "$JSON_PATH/error.json";
+  static const String success = "$JSON_PATH/success.json";
 }
